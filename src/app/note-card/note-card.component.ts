87d7df4bd3b0216baea47c42e58d94753c7c33b2
Yousra Kennou <yousra_kennou@yahoo.com>
2021-04-24 22:21:59 +0100

    Styling note component - part2

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
-  styleUrls: ['./note-card.component.css']
+  styleUrls: ['./note-card.component.css'],
 })
 export class NoteCardComponent implements OnInit {
+  @Input() title: string;
+  @Input() body: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @ViewChild('truncator', { static: true }) truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText', { static: true }) bodyText: ElementRef<HTMLElement>;
+  constructor(private renderer: Renderer2) {}
+  ngOnInit() {
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHight = parseInt(style.getPropertyValue('height'), 10);
+    if (this.bodyText.nativeElement.scrollHeight > viewableHight) {
+      //if there is a text overflow, show the fade out truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      // hide the fade out truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    }
   }
-
 }
